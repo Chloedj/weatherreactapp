6309decf08d62a49d26e0e0f029a5923e06be73a
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-02-12 16:32:29 +0000

    imported code from previous js app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   <div className="weather-app">
+      <header>
+        <form className="search-form" id="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            required
+            id="search-form-input"
+            className="search-form-input"
+          />
+          <input type="submit" value="Search" className="search-form-button" />
+        </form>
       </header>
-    </div>
-  );
+      <main>
+        <div className="weather-app-data">
+          <div>
+            <h1 className="weather-app-city" id="city">Northampton</h1>
+            <p className="weather-app-details">
+              <span id="time">Tuesday 16:30</span>,
+              <span id="description">Partly Sunny</span>
+              <br />
+              Humidity: <strong id="humidity">66</strong>, Wind:
+              <strong id="wind-speed">18</strong>
+            </p>
+          </div>
+          <div className="weather-app-temperature-container">
+            <div id="icon"></div>
+            <div className="weather-app-temperature" id="temperature">8</div>
+            <div className="weather-app-unit">°C</div>
+          </div>
+        </div>
+        <div className="weather-forecast" id="forecast"></div>
+      </main>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Chloedj" target="_blank"
+          >Chloe Duggan-Jones</a
+        >
+        {" "}and is{" "}
+        <a
+          href="https://github.com/Chloedj/Project-weather-app"
+          target="_blank"
+        >
+          on GitHub</a
+        >
+        {" "}and{" "}
+        <a
+          href="https://effortless-empanada-eec07e.netlify.app/"
+          target="_blank"
+          >hosted on Netlify</a
+        >
+      </footer>
+      </div>
+      )
 }
 
 export default App;
