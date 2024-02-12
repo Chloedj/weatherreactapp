ad1e776ab00e256d644716324ad910029425295e
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-02-12 16:36:08 +0000

    added rel="noreferrer" to links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -39,20 +39,20 @@ function App() {
       </main>
       <footer>
         This project was coded by{" "}
-        <a href="https://github.com/Chloedj" target="_blank"
+        <a href="https://github.com/Chloedj" target="_blank" rel="noreferrer"
           >Chloe Duggan-Jones</a
         >
         {" "}and is{" "}
         <a
           href="https://github.com/Chloedj/Project-weather-app"
-          target="_blank"
+          target="_blank" rel="noreferrer"
         >
           on GitHub</a
         >
         {" "}and{" "}
         <a
           href="https://effortless-empanada-eec07e.netlify.app/"
-          target="_blank"
+          target="_blank" rel="noreferrer"
           >hosted on Netlify</a
         >
       </footer>
